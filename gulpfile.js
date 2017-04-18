5b225797c2fd572ca51dcf16418d64ae96b66f86
Bruno Lucas <bruno-lucas@msn.com>
2017-04-18 06:45:32 -0300

    Adicionado o gulp-livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
 
 // task para o sass
 gulp.task('sass', function(){
     return gulp.src('sass/**/*.sass')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(livereload());
 });
 
 // task para o watch
 gulp.task('watch', function(){
+    livereload.listen();
     gulp.watch('sass/**/*.sass', ['sass']);
 });
 // task padrao do gulp
